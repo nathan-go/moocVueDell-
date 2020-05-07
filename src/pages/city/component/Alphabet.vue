d9dbfc6f25fe95f84b2d3fe671ce43087662df59
nathan-go <tigerhao1018@gmail.com>
2020-05-07 18:02:58 +0800

    finish 8-4

---
+++ src/pages/city/component/Alphabet.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'CityAlphabet',
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/variables.styl';
+.list
+  position: absolute
+  flex-direction column
+  justify-content center
+  text-align center
+  display flex
+  top 1.58rem
+  right 0
+  bottom 0
+  width .4rem
+  .item
+    line-height .42rem
+    color $bgColor
+</style>
---
+++ src/pages/city/component/List.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">当前城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">热门城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">北京</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll';
+export default {
+    name: 'CityList',
+    mounted() {
+        this.scroll = new Bscroll(this.$refs.wrapper);
+    },
+};
+</script>
+
+<style lang="stylus" scoped>
+.border-topbottom, .border-bottom
+  &:before
+    border-color #ccc
+  &:after
+    border-color #ccc
+.list
+  position absolute
+  left 0
+  right 0
+  top 1.58rem
+  bottom 0
+  overflow: hidden
+  .title
+    line-height .54rem
+    background #eee
+    padding-left .2rem
+    color #666
+  .button-list
+    overflow: hidden
+    padding .1rem .6rem .1rem .1rem
+    .button-wrapper
+      float left
+      width 33.33%
+      .button
+        text-align center
+        margin .1rem
+        border: .02rem solid #ccc
+        padding .1rem 0
+        border-radius .06rem
+  .item-list
+    color: #666666
+    line-height .76rem
+    padding: 0 .2rem
+</style>
