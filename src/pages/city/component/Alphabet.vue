2a720d5332136f9cccefed33c4bcca4ec0c87f47
nathan-go <tigerhao1018@gmail.com>
2020-05-07 19:08:55 +0800

    finish 8-6

--- src/pages/city/component/Alphabet.vue
+++ src/pages/city/component/Alphabet.vue
@@ -1,6 +1,17 @@
 <template>
     <ul class="list">
-        <li class="item" v-for="(item, key) of cities" :key="key">{{ key }}</li>
+        <li
+            class="item"
+            v-for="(item, key) of letters"
+            :key="key"
+            @click="handleLetterClick"
+            @touchstart="handleTouchStart"
+            @touchmove="handleTouchMove"
+            @touchend="handleTouchEnd"
+            :ref="item"
+        >
+            {{ item }}
+        </li>
     </ul>
 </template>
 
@@ -9,6 +20,41 @@ export default {
     name: 'CityAlphabet',
     props: {
         cities: Object,
+    },
+    data() {
+        return {
+            touchStatus: false,
+        };
+    },
+    computed: {
+        letters() {
+            const letters = [];
+            for (let i in this.cities) {
+                letters.push(i);
+            }
+            return letters;
+        },
+    },
+    methods: {
+        handleLetterClick(e) {
+            this.$emit('change', e.target.innerText);
+        },
+        handleTouchStart() {
+            this.touchStatus = true;
+        },
+        handleTouchEnd() {
+            this.touchStatus = false;
+        },
+        handleTouchMove(e) {
+            if (this.touchStatus) {
+                const startY = this.$refs['A'][0].offsetTop;
+                const touchY = e.touches[0].clientY - 79;
+                const index = Math.floor((touchY - startY) / 20);
+                if (index >= 0 && index < this.letters.length) {
+                    this.$emit('change', this.letters[index]);
+                }
+            }
+        },
     },
 };
 </script>
@@ -26,6 +72,6 @@ export default {
   bottom 0
   width .4rem
   .item
-    line-height .42rem
+    line-height .4rem
     color $bgColor
 </style>
--- src/pages/city/component/List.vue
+++ src/pages/city/component/List.vue
@@ -21,7 +21,12 @@
                     </div>
                 </div>
             </div>
-            <div class="area" v-for="(item, key) of cities" :key="key">
+            <div
+                class="area"
+                v-for="(item, key) of cities"
+                :key="key"
+                :ref="key"
+            >
                 <div class="title border-topbottom">{{ key }}</div>
                 <div class="item-list">
                     <div
@@ -47,6 +52,15 @@ export default {
     props: {
         cities: Object,
         hot: Array,
+        letter: String,
+    },
+    watch: {
+        letter() {
+            if (this.letter) {
+                const element = this.$refs[this.letter][0];
+                this.scroll.scrollToElement(element);
+            }
+        },
     },
 };
 </script>
