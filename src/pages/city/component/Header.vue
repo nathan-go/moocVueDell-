404b85e85a29fc404c1325b44c3291ab40a8f721
nathan-go <tigerhao1018@gmail.com>
2020-05-07 16:52:30 +0800

    finish 8-1

---
+++ src/pages/city/component/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="header">
+        城市选择
+        <router-link to="/">
+            <div class="iconfont header-back">&#xe624;</div>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CityHeader',
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/variables.styl'
+.header
+  position: relative
+  overflow: hidden
+  height: .86rem
+  line-height .86rem
+  text-align: center
+  color: #fff
+  background-color $bgColor
+  font-size: .32rem
+  .header-back
+    width: .64rem
+    font-size: .4rem
+    text-align: center
+    position: absolute
+    left: 0
+    top: 0
+    color: #fff
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,25 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../pages/home/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../pages/home/Home.vue';
+import City from '../pages/city/City.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/city',
+        name: 'City',
+        component: City,
+    },
 ];
 
 const router = new VueRouter({
-  routes
+    routes,
 });
 
 export default router;
