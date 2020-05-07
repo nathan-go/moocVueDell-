56ed71d6a42086332a18199d6913b0c305be5cfa
nathan-go <tigerhao1018@gmail.com>
2020-05-07 17:02:53 +0800

    finish 8-2

---
+++ src/pages/city/component/Search.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="search">
+        <input
+            type="text"
+            class="search-input"
+            placeholder="输入城市名或拼音"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CitySearch',
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/variables.styl';
+.search
+  height .72rem
+  background-color $bgColor
+  padding 0 .1rem
+  .search-input
+    box-sizing border-box
+    height .62rem
+    line-height .62rem
+    width: 100%
+    text-align: center
+    border-radius: .06rem
+    color: #666
+    padding 0 .1rem
+</style>
