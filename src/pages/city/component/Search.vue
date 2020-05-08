a9a8b35a378faa5f36ff2ecad2a4c9d8e9c342a3
nathan-go <tigerhao1018@gmail.com>
2020-05-08 10:41:29 +0800

    finish 8-9

--- src/pages/city/component/Search.vue
+++ src/pages/city/component/Search.vue
@@ -1,16 +1,81 @@
 <template>
-    <div class="search">
-        <input
-            type="text"
-            class="search-input"
-            placeholder="输入城市名或拼音"
-        />
+    <div>
+        <div class="search">
+            <input
+                type="text"
+                class="search-input"
+                placeholder="输入城市名或拼音"
+                v-model="keyword"
+            />
+        </div>
+        <div class="search-content" ref="search" v-show="keyword">
+            <ul>
+                <li
+                    class="search-item border-bottom"
+                    v-for="item of list"
+                    :key="item.id"
+                >
+                    {{ item.name }}
+                </li>
+                <li
+                    class="search-item border-bottom no-result"
+                    v-show="hasSearchResult"
+                >
+                    没有找到匹配数据
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll';
 export default {
     name: 'CitySearch',
+    data() {
+        return {
+            keyword: '',
+            list: [],
+            timer: null,
+        };
+    },
+    props: {
+        cities: Object,
+    },
+    watch: {
+        keyword() {
+            // 监听keyword的变化，
+            if (this.timer) {
+                clearTimeout(this.timer);
+            }
+            if (!this.keyword) {
+                this.list = [];
+                return;
+            }
+            this.timer = setTimeout(() => {
+                const result = [];
+                for (let i in this.cities) {
+                    this.cities[i].forEach((value) => {
+                        if (
+                            value.spell.indexOf(this.keyword) > -1 ||
+                            value.name.indexOf(this.keyword) > -1
+                        ) {
+                            result.push(value);
+                        }
+                    });
+                }
+                this.list = result;
+            }, 100);
+        },
+    },
+    computed: {
+        hasSearchResult() {
+            return !this.list.length && this.keyword;
+        },
+    },
+    mounted() {
+        this.scroll = new Bscroll(this.$refs.search);
+    },
 };
 </script>
 
@@ -29,4 +94,20 @@ export default {
     border-radius: .06rem
     color: #666
     padding 0 .1rem
+.search-content
+  z-index 1
+  overflow: hidden
+  position: absolute
+  top 1.58rem
+  left 0
+  right 0
+  bottom 0
+  background-color #eee
+  .search-item
+    line-height .62rem
+    padding-left .2rem
+    color: #666
+    background-color #fff
+  .no-result
+    background-color orange
 </style>
